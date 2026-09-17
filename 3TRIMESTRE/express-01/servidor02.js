9a016a120df41cbc3a889ec9a7bba2e73febe09b
fernanda <nanda.fst08@gmail.com>
2026-09-17 13:28:18 +0000

    Ajusta formatação e corrige erros de sintaxe nos arquivos HTML, CSS e servidor

--- 3TRIMESTRE/express-01/servidor02.js
+++ 3TRIMESTRE/express-01/servidor02.js
@@ -1,12 +1,15 @@
-import express from  'express'
+import express from 'express'
+import path from 'path'
 
+const dirBase = import.meta.dirname
 const app = express()
 const porta = 3000
-//Criar as rotas do servidor
+//Usar Middleware(Guardião)
+app.use(express.static(path.join(dirBase, 'publico')))
+
+// Criar as rotas do servidor
 app.get('/', (req, res) => {
-    res.send('/paginas/index.html,'{root: import.meta.dirname})
+    res.sendFile('/paginas/index.html',{ root: dirBase })
 })
-
-
-//Liberar a porta do meu computador 
-app.listen(porta, () => {console.log('Servidor está vivo!')} ) 
+// Liberar a porta do meu computador 
+app.listen(porta, () => { console.log('Servidor está vivo!') }) 
